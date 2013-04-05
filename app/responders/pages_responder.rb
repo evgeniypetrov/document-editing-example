a3ac528238849909c5f6e82e22375833feb8ad19
Evgeniy <malleus.petrov@gmail.com>
2013-04-05 14:36:18 +0400

    rendering static first page

---
+++ app/responders/pages_responder.rb
@@ -0,0 +1,22 @@
+class PagesResponder < BaseResponder
+  class RenderingScope
+    include Webmate::Views::Helpers
+    include Sinatra::Cookies
+    include Sinatra::Sprockets::Helpers
+
+    def user_websocket_token
+      '123'
+    end
+  end
+
+  def index
+    #slim :"pages/index.html", layout: :'layouts/application.html'
+    layout =  File.open('app/views/layouts/application.html.slim', 'rb').read
+    contents =  File.open('app/views/pages/index.html.slim', 'rb').read
+
+    l = Slim::Template.new { layout }
+    c = Slim::Template.new { contents }.render(RenderingScope.new)
+
+    l.render(RenderingScope.new) { c }
+  end
+end

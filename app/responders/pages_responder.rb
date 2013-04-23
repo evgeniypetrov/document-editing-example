80fd1666fbd660f4a6a81212a49cf4552928f733
Evgeniy <malleus.petrov@gmail.com>
2013-04-23 21:13:00 +0400

    updated code to use webmate slim engine. not directly slim gem

--- app/responders/pages_responder.rb
+++ app/responders/pages_responder.rb
@@ -1,35 +1,7 @@
 class PagesResponder < BaseResponder
-  class RenderingScope
-    include Webmate::Views::Helpers
-    include Sinatra::Cookies
-    include Sinatra::Sprockets::Helpers
-
-    def initialize(responder)
-      @responder = responder
-    end
-
-    def user_websocket_token
-    end
-
-    def current_user
-      @responder.request.env['warden'].user('user')
-    end
-
-    def current_user_id
-      @responder.request.env['warden'].user('user').id
-    end
-  end
+  include Webmate::Responders::Templates
 
   def index
-    #@request.env['warden'].env['rack.session']
-
-    #slim :"pages/index.html", layout: :'layouts/application.html'
-    layout =  File.open('app/views/layouts/application.html.slim', 'rb').read
-    contents =  File.open('app/views/pages/index.html.slim', 'rb').read
-
-    l = Slim::Template.new { layout }
-    c = Slim::Template.new { contents }.render(RenderingScope.new(self))
-
-    l.render(RenderingScope.new(self)) { c }
+    slim :index, layout: 'application'
   end
 end
